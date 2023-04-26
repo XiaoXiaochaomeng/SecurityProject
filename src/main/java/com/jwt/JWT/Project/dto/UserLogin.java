9763ab40f77ad41d867f631dbd7cbc64f345c288
Minh Phan <minh.phan@ntq-solution.com.vn>
2023-04-26 17:21:42 +0700

    update

---
+++ src/main/java/com/jwt/JWT/Project/dto/UserLogin.java
@@ -0,0 +1,15 @@
+package com.jwt.JWT.Project.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserLogin {
+    // Thong tin user name
+    private String username;
+    // Thong tin password
+    private String password;
+}
